7256a0b695ed0a4449eee03f37fc4c496f230a7f
Sean Griffin <sean@thoughtbot.com>
2015-06-14 18:56:24 -0600

    Bubble up errors rather than `panic!`ing
    While `.unwrap()` on a `Result` will cause the program to `panic!` if it
    fails for some reason, `try!` will simply cause the error to bubble up.
    For this spike, we're still going to `panic!` at the top level, if an
    error occurs at any point, but this will make more of the code easy to
    use should this end up going into a real app.
    
    Side note: The conversion between `Vec<Result<T>>` and `Result<Vec<T>>`
    (possibly more, and equivalent to `sequence` in Haskell?) is simply
    handled by the type system, and not something we have to write
    explicitly.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate byteorder;
 
 use byteorder::{ReadBytesExt, LittleEndian};
 
-use std::io;
+use std::io::{Read, Result};
 use std::slice;
 use std::fs::File;
 use std::mem;
@@ -24,7 +24,7 @@ struct AnimationFile {
 fn main() {
     let mut f = File::open("model.anim").unwrap();
     let animation_file;
-    unsafe { animation_file = parse_animation_file(&mut f); }
+    unsafe { animation_file = parse_animation_file(&mut f).unwrap(); }
     println!("[{}, {}, {}]", animation_file.meshes[0].vertices[0], animation_file.meshes[0].vertices[1], animation_file.meshes[0].vertices[2]);
     println!("[{}, {}]", animation_file.meshes[0].uvs[0], animation_file.meshes[0].uvs[1]);
     println!("[{}, {}, {}]", animation_file.meshes[0].normals[0], animation_file.meshes[0].normals[1], animation_file.meshes[0].normals[2]);
@@ -32,57 +32,58 @@ fn main() {
     println!("[{}, {}, {}]", animation_file.meshes[0].skin_indices[0], animation_file.meshes[0].skin_indices[1], animation_file.meshes[0].skin_indices[2]);
 }
 
-unsafe fn parse_animation_file<R: io::Read>(reader: &mut R) -> AnimationFile {
-    let _version = reader.read_i32::<LittleEndian>().unwrap();
-    let num_meshes = reader.read_i32::<LittleEndian>().unwrap();
-    let meshes = (0..num_meshes).map(|_i| {
+unsafe fn parse_animation_file<R: Read>(reader: &mut R) -> Result<AnimationFile> {
+    let _version = try!(reader.read_i32::<LittleEndian>());
+    let num_meshes = try!(reader.read_i32::<LittleEndian>());;
+    let meshes = try!((0..num_meshes).map(|_i| {
         parse_mesh(reader)
-    }).collect();
-    AnimationFile { meshes: meshes }
+    }).collect());
+    Ok(AnimationFile { meshes: meshes })
 }
 
-unsafe fn parse_mesh<R: io::Read>(reader: &mut R) -> Mesh {
-    let vertices = read_array(reader);
-    let short_uvs: Vec<i16> = read_array(reader);
+unsafe fn parse_mesh<R: Read>(reader: &mut R) -> Result<Mesh> {
+    let vertices = try!(read_array(reader));
+    let short_uvs: Vec<i16> = try!(read_array(reader));
     let uvs = short_uvs.iter().map(|&uv| (uv as f32) / 4096.0).collect();
-    let normals = read_array(reader);
-    let faces = read_faces(reader);
-    let skin_weights = read_array(reader);
-    let skin_indices = read_array(reader);
+    let normals = try!(read_array(reader));
+    let faces = try!(read_faces(reader));
+    let skin_weights = try!(read_array(reader));
+    let skin_indices = try!(read_array(reader));
 
-    Mesh {
+    Ok(Mesh {
         vertices: vertices,
         uvs: uvs,
         normals: normals,
         elements: faces,
         skin_weights: skin_weights,
         skin_indices: skin_indices,
-    }
+    })
 }
 
-unsafe fn read_array<T: Sized, R: io::Read>(reader: &mut R) -> Vec<T> {
-    let num_elements = reader.read_i32::<LittleEndian>().unwrap() as usize;
+unsafe fn read_array<T: Sized, R: Read>(reader: &mut R) -> Result<Vec<T>> {
+    let num_elements = try!(reader.read_i32::<LittleEndian>()) as usize;
     let mut result = Vec::with_capacity(num_elements);
     result.set_len(num_elements);
     let num_bytes = num_elements * mem::size_of::<T>();
-    read_bytes(reader, result.as_mut_ptr() as *mut u8, num_bytes);
-    result
+    try!(read_bytes(reader, result.as_mut_ptr() as *mut u8, num_bytes));
+    Ok(result)
 }
 
-unsafe fn read_bytes<R: io::Read>(reader: &mut R, ptr: *mut u8, bytes_to_read: usize) {
+unsafe fn read_bytes<R: Read>(reader: &mut R, ptr: *mut u8, bytes_to_read: usize) -> Result<()> {
     let buf = slice::from_raw_parts_mut(ptr, bytes_to_read);
     let mut bytes_read = 0;
     while bytes_read < bytes_to_read {
-        bytes_read += reader.read(&mut buf[bytes_read..]).unwrap();
+        bytes_read += try!(reader.read(&mut buf[bytes_read..]));
     }
+    Ok(())
 }
 
-unsafe fn read_faces<R: io::Read>(reader: &mut R) -> HashMap<u32, Vec<u16>> {
-    let num_face_sets = reader.read_i32::<LittleEndian>().unwrap();
+unsafe fn read_faces<R: Read>(reader: &mut R) -> Result<HashMap<u32, Vec<u16>>> {
+    let num_face_sets = try!(reader.read_i32::<LittleEndian>());
     (0..num_face_sets).map(|_i| {
-        let material_index = reader.read_u32::<LittleEndian>().unwrap();
-        let quads: Vec<u16> = read_array(reader);
-        let mut triangles: Vec<u16> = read_array(reader);
+        let material_index = try!(reader.read_u32::<LittleEndian>());
+        let quads: Vec<u16> = try!(read_array(reader));
+        let mut triangles: Vec<u16> = try!(read_array(reader));
         triangles.reserve((quads.len() as f32 * 1.5) as usize);
 
         // I'm sure there's an iterator for this but I can't seem to find it.
@@ -97,6 +98,6 @@ unsafe fn read_faces<R: io::Read>(reader: &mut R) -> HashMap<u32, Vec<u16>> {
             i += 4;
         }
 
-        (material_index, triangles)
-    }).collect()
+        Ok((material_index, triangles))
+    }).collect::<Result<HashMap<u32, Vec<u16>>>>()
 }
